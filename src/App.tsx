ec0d5e61bf903c68b51456c3d6b7aeef6f1b56cd
Timofey Pichugin <tpich87@gmail.com>
2023-09-23 12:54:51 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components'
+
+function App() {
+  const [current, setCurrent] = React.useState('one')
+  return (
+    <div className="App" style={{ display: 'flex' }}>
+      
+    </div>
+  );
+}
+
+export default App;
